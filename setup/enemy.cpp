7eb781214759fe204e56ed7ee7a7843ca5093057
ptrChar <CharlesOfficial1@hotmail.com>
2021-02-14 23:25:52 -0500

    bug fixes

--- setup/enemy.cpp
+++ setup/enemy.cpp
@@ -33,17 +33,31 @@ int Enemy::randomNumberGenerator(const int &lowerBound, const int &upperBound)
 //move the enemy
 void Enemy::move()
 {
-    int prevCoordinate[2];
-    prevCoordinate[axis::x] = this->coordinate[axis::x];
-    prevCoordinate[axis::y] = this->coordinate[axis::y];
+    int prevCoord[2];
+    prevCoord[axis::x] = this->coordinate[axis::x];
+    prevCoord[axis::y] = this->coordinate[axis::y];
 
+    //if 0, move in the x direction
+    //if y, move in the y direction
     int move = randomNumberGenerator(0,1);
 
+    //if 0, move -1 in the x direction
+    //if 1, move +1 in the x direction
     int moveX_decider = randomNumberGenerator(0,1);
+
+    //if 0, move -1 in the y direction
+    //if 1, move +1 in the y direction
     int moveY_decider = randomNumberGenerator(0,1);
 
     if(move == axis::x) {moveX_decider == 0 ? coordinate[axis::x] -= 1 : coordinate[axis::x] += 1;}
     else {moveY_decider == 0 ? coordinate[axis::y] -= 1 : coordinate[axis::y] += 1;}
+
+    //if the enemy move outside the map, it will teleport them back to their previous location
+    if(this->coordinate[axis::x] < 0 || this->coordinate[axis::x] >= (MAX_X-1) || this->coordinate[axis::y] <= 0 || this->coordinate[axis::y] >= (MAX_Y-1))
+    {
+        coordinate[axis::x] = prevCoord[axis::x];
+        coordinate[axis::y] = prevCoord[axis::y];
+    }
 
 }
 
